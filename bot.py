6ae97d01dd36f7e65d2e34873aa6318368f52a4e
Dennis <blinhic850@mail.ru>
2020-11-28 19:06:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -83,8 +83,9 @@ def text(message):
             schedule = schedule.replace("['", '').replace("']", '').replace("', '", '\n')
             keyboard = types.ReplyKeyboardMarkup(row_width = 1, resize_keyboard = True)
             button = types.KeyboardButton(text = "Расписание на сегодня")
-            keyboard.add(button)
-            bot.send_message(message.chat.id, schedule, parse_mode = 'Markdown', reply_markup = keyboard)
+            button1 = types.KeyboardButton(text = "Оставить пожелание")
+        keyboard.add(button, button1)
+        bot.send_message(message.chat.id, schedule, parse_mode = 'Markdown', reply_markup = keyboard)
         elif parity == 1:
             data_loads = json.load(open('./schedule.json'))
             data = json.dumps(data_loads)
@@ -97,8 +98,9 @@ def text(message):
             schedule = schedule.replace("['", '').replace("']", '').replace("', '", '\n')
             keyboard = types.ReplyKeyboardMarkup(row_width = 1, resize_keyboard = True)
             button = types.KeyboardButton(text = "Расписание на сегодня")
-            keyboard.add(button)
-            bot.send_message(message.chat.id, schedule, parse_mode = 'Markdown', reply_markup = keyboard)
+            button1 = types.KeyboardButton(text = "Оставить пожелание")
+        keyboard.add(button, button1)
+        bot.send_message(message.chat.id, schedule, parse_mode = 'Markdown', reply_markup = keyboard)
         elif message.text == 'Оставить пожелание':
             bot.send_message(message.chat.id, "По всем вопросам и пожеланиям обращаться к @whomet")
 if __name__ == '__main__':
