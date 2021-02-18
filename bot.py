2e9a2857a9ba8271fb7d0d3a3713b733e3b1ecba
Dennis <blinhic850@mail.ru>
2021-02-18 20:09:55 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -476,8 +476,6 @@ def text(message):
         keyboard.row(button3, button4)
         keyboard.row(button2)
         bot.send_message(message.chat.id, message_password, reply_markup = keyboard)
-        time.sleep(5)
-        bot.delete_message(message.chat.id, message.message_id + 1)
     elif message.text == 'В меню расписаний' or message.text == 'Вернуться назад':
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard = True)
         button = types.KeyboardButton(text = "Расписание на сегодня")
