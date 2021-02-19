4ea0a1a2234c2a261c8c6a58d634edaa33ea68e7
Dennis <blinhic850@mail.ru>
2021-02-19 15:56:50 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -258,7 +258,7 @@ def callback_inline(call):
             keyboard = types.InlineKeyboardMarkup()
             button = types.InlineKeyboardButton(text = "Сортировка по фамилиям", callback_data = 'family')
             keyboard.add(button) 
-            bot.edit_message_text(chat_id = call.message.chat.id, message_id = call.message.message_id, text = sorted(rating), parse_mode = 'Markdown', reply_markup = keyboard)
+            bot.edit_message_text(chat_id = call.message.chat.id, message_id = call.message.message_id, text = sorted(rating.values()), parse_mode = 'Markdown', reply_markup = keyboard)
         elif call.data == 'family':
             keyboard = types.InlineKeyboardMarkup()
             button = types.InlineKeyboardButton(text = "Сортировка по процентам", callback_data = 'percent')
