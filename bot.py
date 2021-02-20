9685b3a8a02b5cab3959e684b26100ebb0b929c7
Dennis <blinhic850@mail.ru>
2021-02-20 11:38:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,13 +31,14 @@ json_data4 = json.loads(data4)
 
 rating = json_data4["Список"]
 percents = json_data4["Проценты"]
+percent_cources_sorted = percents.values()
+print(sorted(percent_cources_sorted))
+
 layout = ''
 key = 0
 for x, y in zip(rating, percents):
     rating_cources = rating.get(str(key))
     percent_cources = percents.get(str(key))
-    percent_cources_sorted = percents.values()
-    print(sorted(percent_cources_sorted))
     key += 1
     layout += f'{rating_cources} {percent_cources}%\n'
             
