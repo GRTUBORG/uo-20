03bee855c811ac2ded09361b394662500cb9f604
Dennis <blinhic850@mail.ru>
2021-02-22 23:25:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -526,8 +526,11 @@ def text(message):
         keyboard.add(button)
         bot.send_message(message.chat.id, "Напиши своё сообщение, а я отправлю его разработчику, \nлибо отправь «Отмена» для отмены!", reply_markup = keyboard)
         bot.register_next_step_handler(message, get_message)
+    #подобие ИИ
     elif re.search(r'\bпривет!', message.text.lower()):
-        bot.send_message(message.chat.id, "Рад тебя видеть!")
+        with open ('./AI/hello.txt', 'r') as file:
+            lines = file.readlines()
+        bot.send_message(message.chat.id, random.choice(lines))
     
 def get_message(message):
     callback = message.text
