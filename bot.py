cc702b0800f765eb142bed43e65df84bb6b87953
Dennis <blinhic850@mail.ru>
2021-03-28 15:27:45 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -583,8 +583,10 @@ def get_message(message):
     else:
         keyboard = types.InlineKeyboardMarkup()
         button = types.InlineKeyboardButton(text = "⬅️ Назад в меню", callback_data = 'back_to_the_menu')
-        keyboard.add(button)
-        bot.send_message(message.chat.id, f"{json_data5['id'][id]['name']}, лови свои логин и пароль! \n\n*Логин:* `{json_data5['id'][id]['login']}` \n*Пароль:* `{json_data5['id'][id]['pass']}` \nСсылка для быстрого перехода на сайт: https://do.unitech-mo.ru", parse_mode = 'Markdown', reply_markup = keyboard)
+        url_button = types.InlineKeyboardButton(text = "Перейти на сайт тестирования ↗️", url = 'https://do.unitech-mo.ru')
+        keyboard.row(button)
+        keyboard.row(url_button)
+        bot.send_message(message.chat.id, f"{json_data5['id'][id]['name']}, лови свои логин и пароль! \n\n*Логин:* `{json_data5['id'][id]['login']}` \n*Пароль:* `{json_data5['id'][id]['pass']}`", parse_mode = 'Markdown', reply_markup = keyboard)
         
 if __name__ == '__main__':
     while True:
