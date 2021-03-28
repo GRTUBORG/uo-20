6170afd96e665909d7f4a462b4785b4c4c49c8e0
Dennis <blinhic850@mail.ru>
2021-03-28 23:48:15 +0300

    Update testing_code.py

--- testing_code.py
+++ testing_code.py
@@ -423,9 +423,12 @@ def callback_inline(call):
             now_next3 = now_next3.strftime("(%d.%m.%y)")
             keyboard = types.InlineKeyboardMarkup()
             button = types.InlineKeyboardButton(text = f"{now_next}", callback_data = 'now_next')
-            button2 = types.InlineKeyboardButton(text = f"{now_next1}", callback_data = 'now_next1')
-            button4 = types.InlineKeyboardButton(text = f"{now_next2}", callback_data = 'now_next2')
-            button5 = types.InlineKeyboardButton(text = f"{now_next3}", callback_data = 'now_next3')
+            button1 = types.InlineKeyboardButton(text = f"{now_next1}", callback_data = 'now_next1')
+            button2 = types.InlineKeyboardButton(text = f"{now_next2}", callback_data = 'now_next2')
+            button3 = types.InlineKeyboardButton(text = f"{now_next3}", callback_data = 'now_next3')
+            keyboard.row(button, button1)
+            keyboard.row(button2, button3)
+            bot.edit_message_text(chat_id = call.message.chat.id, message_id = call.message.message_id, text = f'*Меню расписания по дням.*', parse_mode = 'Markdown', reply_markup = keyboard)
         
         elif call.data == 'rating_by_course':
             keyboard = types.InlineKeyboardMarkup()
