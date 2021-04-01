a8cf243e144c7252f919dd8b0291272d62817852
Dennis <blinhic850@mail.ru>
2021-04-01 11:55:07 +0300

    Update testing_code.py

--- testing_code.py
+++ testing_code.py
@@ -334,7 +334,7 @@ def callback_inline(call):
             global page_list
             page_list = 1
             keyboard = types.InlineKeyboardMarkup()
-            button_page1 = types.InlineKeyboardButton(text = f"Страница {page_list + 1}", callback_data = f'page{page_list + 1}')
+            button_page1 = types.InlineKeyboardButton(text = f"Страница {page_list}", callback_data = f'page{page_list + 1}')
             button1 = types.InlineKeyboardButton(text = "Вперёд", callback_data = 'further')
             button = types.InlineKeyboardButton(text = "⬅️ Назад в материалы по английскому языку", callback_data = 'english')
             keyboard.row(button_page1, button1)
@@ -343,7 +343,7 @@ def callback_inline(call):
         elif call.data == 'further':
             page_list += 1
             keyboard = types.InlineKeyboardMarkup()
-            button_page1 = types.InlineKeyboardButton(text = f"Страница {page_list + 1}", callback_data = f'page{page_list + 1}')
+            button_page1 = types.InlineKeyboardButton(text = f"Страница {page_list}", callback_data = f'page{page_list + 1}')
             button1 = types.InlineKeyboardButton(text = "Вперёд", callback_data = 'further')
             button = types.InlineKeyboardButton(text = "⬅️ Назад в материалы по английскому языку", callback_data = 'english')
             keyboard.row(button_page1, button1)
